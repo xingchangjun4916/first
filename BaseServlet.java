c0853229ac169057c0a2fd1bc7cd2267cbae71bc
changjun <changjun4916@163.com>
2018-01-08 21:32:48 +0800

    201818

--- BaseServlet.java
+++ BaseServlet.java
@@ -11,9 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class BaseServlet extends HttpServlet {
 	private Long age;
 	private Long age;
-	private Long age;
-	private Long age;
-	private Long age;
+
 	@Override
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
